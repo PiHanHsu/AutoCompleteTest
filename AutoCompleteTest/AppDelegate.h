494af0ae62c96d7efbe8edda228b1d59f2879c5a
PiHanHsu <pihanhsu@gmail.com>
2014-11-28 16:05:17 +0800

    Initial Commit

---
+++ AutoCompleteTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoCompleteTest
+//
+//  Created by PiHan Hsu on 2014/11/28.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoCompleteTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoCompleteTest
+//
+//  Created by PiHan Hsu on 2014/11/28.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
